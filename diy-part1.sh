f085f39d31366cf07e2cc9744f5cf2274d209666
gsy-tt <mailgsy@163.com>
2025-06-23 20:49:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,15 +11,16 @@ echo "src-git small https://gitee.com/kenzok8/small" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
 
+# 添加缺失的依赖源
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+echo "src-git other https://github.com/Lienol/openwrt-packages.git" >> feeds.conf.default
+
 # 添加luci-theme-argon主题（国内加速）
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
-# 优化：添加IPQ5332专用软件包源
-echo "src-git ipq5332 https://github.com/immortalwrt/packages.git;openwrt-23.05" >> feeds.conf.default
-
-# 优化：添加额外的网络加速模块
-echo "src-git turboacc https://github.com/chenmozhijin/turboacc.git" >> feeds.conf.default
-
-# 优化：添加SmartDNS
+# 添加SmartDNS
 echo "src-git smartdns https://github.com/pymumu/luci-app-smartdns.git;lede" >> feeds.conf.default
+
+# 添加upx/host支持
+echo "src-git utils https://github.com/openwrt/packages.git;openwrt-23.05" >> feeds.conf.default
