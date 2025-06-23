2c21b705bacbdc9b9a24b402f7bc904ec05c0853
gsy-tt <mailgsy@163.com>
2025-06-23 20:56:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-# 脚本功能：添加软件源和主题，配置镜像加速
+# 脚本功能：添加软件源和主题，配置镜像加速（去重优化）
 
 # 使用清华大学镜像源
 sed -i 's#https://git.openwrt.org#https://github.com/openwrt#' include/feeds.mk
 sed -i 's#http://downloads.openwrt.org#https://mirrors.tuna.tsinghua.edu.cn/openwrt#' feeds.conf.default
 
-# 添加常用软件包源（国内优化）
+# 添加常用软件包源（国内优化，去重处理）
 echo "src-git kenzo https://gitee.com/kenzok8/openwrt-packages" >> feeds.conf.default
 echo "src-git small https://gitee.com/kenzok8/small" >> feeds.conf.default
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-
-# 添加缺失的依赖源
-echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
-echo "src-git other https://github.com/Lienol/openwrt-packages.git" >> feeds.conf.default
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default  # 仅保留一个lienol源
 
 # 添加luci-theme-argon主题（国内加速）
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
@@ -22,5 +19,5 @@ git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-ar
 # 添加SmartDNS
 echo "src-git smartdns https://github.com/pymumu/luci-app-smartdns.git;lede" >> feeds.conf.default
 
-# 添加upx/host支持
+# 添加upx/host支持（明确源）
 echo "src-git utils https://github.com/openwrt/packages.git;openwrt-23.05" >> feeds.conf.default
